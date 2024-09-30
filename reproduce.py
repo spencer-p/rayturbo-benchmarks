277da051c85e8468a3a82d62e687755db8091f0f
Richard Liaw <rliaw@berkeley.edu>
2024-09-30 13:50:33 -0700

    Create reproduce.py

---
+++ reproduce.py
@@ -0,0 +1,26 @@
+import time
+import ray
+from ray.data._internal.execution.util import memory_string
+
+def benchmark(ds_factory):
+    start_time = time.time()
+
+    ds = ds_factory()
+
+    nbytes = 0 
+    time_to_first_bundle = None
+    for bundle in ds.iter_internal_ref_bundles():
+        nbytes += bundle.size_bytes()
+        if time_to_first_bundle is None:
+            time_to_first_bundle = time.time() - start_time
+
+    print(f"Read {memory_string(nbytes)} in {time.time() - start_time:.02f}s")
+    print(f"First bundle in {time_to_first_bundle:.02f}s")
+
+
+# S3 scales to high request rates. To ensure that the second method called doesn't have 
+# a performance advantage, I'm doing a "warmup".
+benchmark(lambda: ray.data.read_parquet(DATASET_URI))
+
+# Benchmark runtime
+benchmark(lambda: ray.data.read_parquet(DATASET_URI)) 
