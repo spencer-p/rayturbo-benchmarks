b95832cfd6244450c599786798090474935567ee
Spencer Peterson <spencerjp@google.com>
2025-07-23 13:17:10 -0700

    Clean cluster create command

--- make-cluster.sh
+++ make-cluster.sh
@@ -2,15 +2,16 @@
 
 # This configures a cluster based on
 # https://www.anyscale.com/blog/rayturbo-data-improvements.
+# max cpu is 16 for head, 5*64 for workers, round up to nearest hundred.
+# max mem is 64+5*256, round up to nearest hundred.
 gcloud container clusters create ray-bench-large \
-  --addons=RayOperator
+  --addons=RayOperator \
   --enable-ray-cluster-logging \
   --enable-ray-cluster-monitoring \
-  --region=us-central1 \
   --enable-autoprovisioning \
-  --max-cpu=400 \  # 16 for head, 5*64 for workers, round up to nearest hundred.
-  --max-memory=1400 \  # 64+5*256, round up to nearest hundred.
-  --machine-type c3 \
+  --max-cpu=400 \
+  --max-memory=1400 \
+  --machine-type c3d-standard-90 \
   --num-nodes 1
 
 kubectl apply -f ray-cluster-bench-large.yaml
